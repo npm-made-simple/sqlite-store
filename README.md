70041e9b0282d076e13bc8a27718c127a1a9c808
alex* <31458482+alexasterisk@users.noreply.github.com>
2024-05-18 22:56:30 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# sqlite-store
-Makes use of Keyv to make an even easier SQLite that also cares about dev/prod environments
+# Simple Logging [![@made-simple/sqlite-store](https://github.com/npm-made-simple/sqlite-store/actions/workflows/publish.yml/badge.svg)](https://github.com/npm-made-simple/sqlite-store/actions/workflows/publish.yml)
+
+---
+
+Using Keyv to store data even easier with SQLite with care for development environments. Extending the functionality of Keyv to copy data from one store to another and wipe development data.
+
+Also with optional typings for what is store in the database, reconciliation, fallbacks, error reverting, and more.
+Keyv naturally uses `async/await` but this package **does not**.
+
+## Installation
+
+```bash
+npm install @made-simple/sqlite-store
+```
+
+## Example Usage
+
+```javascript
+const Store = require('@made-simple/sqlite-store');
+
+const template = {
+    foo: "bar",
+    bar: 6
+}
+
+const store = new Store<typeof template>("path/to/database");
+store.reconcile(template);
+
+store.set("foo", "bar");
+store.get("bar", 6); // will be 6 if not found, this will be set in the database too
+store.copyFrom(store2, true) // 2nd argument is if it should start from a clean store
+store.copyKey(store2, "foo", "bar") // attempts to copy the key foo from store2 to store, if not found, it will set it to bar
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change. [Repository](https://github.cmo/npm-made-simple/sqlite-store)
+
+## Contributors
+
+[@alexasterisk](https://github.com/alexasterisk)
---
+++ src/index.ts
@@ -0,0 +1,286 @@
+import Keyv from "keyv";
+
+/**
+ * Wrapper for Keyv SQLite databases.
+ * @example
+ * const store = new Store("db/store");
+ * store.set("key", "value");
+ * store.get("key"); // "value"
+ */
+export default class Store<T extends {} = {}> {
+    protected readonly inDevMode: boolean = false;
+    readonly uri: string;
+    protected keyv: Keyv | null = null;
+    protected store: T = {} as T;
+    private connected_: boolean = false;
+
+    get internalStore(): T {
+        return this.store;
+    }
+
+    get connected(): boolean {
+        return this.connected_ && this.keyv !== null;
+    }
+
+    /**
+     * Creates an instance of Store.
+     * @param {string} uri The URI of the database.
+     * @param {boolean} clean Whether to clear the database. Optional.
+     * @example
+     * const store = new Store("db/store");
+     */
+    constructor(uri: string, clean?: boolean) {
+        this.uri = `sqlite://${uri}.sqlite`;
+        if (process.env.NODE_ENV === "development") this.inDevMode = true;
+
+        this.connect();
+        if (clean) this.clear();
+    }
+
+    /**
+     * Adds a listener to the database.
+     * Returns `undefined` if the database is not connected.
+     * @param {string | symbol} event The event to listen to.
+     * @param {(...args: any[]) => void} listener The listener to add.
+     * @returns {true | undefined}
+     * @example
+     * store.addListener("error", console.error);
+     */
+    addListener(event: string | symbol, listener: (...args: any[]) => void): true | undefined {
+        if (!this.connected) return undefined;
+        this.keyv!.addListener(event, listener);
+        return true;
+    }
+
+    /**
+     * Connects to the database.
+     * @returns {Promise<void>}
+     * @example
+     * this.connect();
+     */
+    protected async connect(): Promise<void> {
+        this.keyv = new Keyv(this.uri);
+        this.keyv.prependListener("error", console.error);
+
+        const data = await this.keyv.get("store") ?? {};
+        this.connected_ = true;
+        this.store = data;
+    }
+
+    /**
+     * Reconnects to the database if disconnected.
+     * @returns {true}
+     * @example
+     * store.reconnect();
+     */
+    reconnect(): true {
+        if (this.connected) return true;
+        this.connect();
+        return true;
+    }
+
+    /**
+     * Disconnects from the database.
+     * Returns `undefined` if the database is already disconnected.
+     * @returns {true | undefined}
+     * @example
+     * store.disconnect(); // true
+     * store.disconnect(); // undefined
+     */
+    disconnect(): true | undefined {
+        if (!this.connected) return undefined;
+        this.keyv!.disconnect().finally(() => {
+            this.store = {} as T;
+            this.connected_ = false;
+        });
+
+        this.keyv = null;
+        return true;
+    }
+
+    /**
+     * Reconciles the database with a template.
+     * Returns `undefined` if the database is not connected.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {T} template The template to reconcile with.
+     * @returns {boolean | undefined}
+     * @example
+     * store.reconcile({ key: "value" });
+     * store.get("key"); // "value"
+     */
+    reconcile(template: T): boolean | undefined {
+        if (!this.connected) return undefined;
+
+        const before = { ...this.store };
+        for (const key in template) {
+            if (!this.has(key)) this.store[key] = template[key];
+        }
+
+        return this.update(before);
+    }
+
+    /**
+     * Clears the database.
+     * Returns `undefined` if the database is not connected.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @returns {boolean | undefined}
+     * @example
+     * store.clear();
+     */
+    clear(): boolean | undefined {
+        if (!this.connected) return undefined;
+
+        const before = { ...this.store };
+        this.store = {} as T;
+        return this.update(before);
+    }
+
+    /**
+     * Deletes a key from the database.
+     * Returns `undefined` if the database is not connected.
+     * Returns `null` if the key does not exist.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {string} key The key to delete.
+     * @returns {boolean | null | undefined}
+     * @example
+     * store.delete("key");
+     * store.get("key"); // null
+     * store.delete("key"); // null
+     */
+    delete(key: string): boolean | null | undefined {
+        if (!this.connected) return undefined;
+        if (!this.has(key)) return null;
+
+        const before = { ...this.store };
+        delete this.store[key];
+        return this.update(before);
+    }
+
+    /**
+     * Gets a value from the database.
+     * If a fallback is provided, it will be set in the database and returned.
+     * If setting the fallback fails, the database is reverted and `null` is returned as if the fallback was never provided.
+     * Returns `undefined` if the database is not connected.
+     * Returns `null` if the key does not exist and no fallback is provided.
+     * @param {string} key The key to get.
+     * @param {T} fallback The fallback value to use. Optional.
+     * @returns {T | null | undefined}
+     * @example
+     * store.get("key"); // null
+     * store.get("key", "value"); // "value"
+     * store.get("key"); // "value"
+     */
+    get<T extends unknown>(key: string, fallback?: T): T | null | undefined {
+        if (!this.connected) return undefined;
+        if (!this.has(key)) {
+            if (fallback === undefined) return null;
+            if (this.set(key, fallback) === false) return null;
+        }
+
+        return this.store[key] as T;
+    }
+
+    /**
+     * Checks if a key exists in the database.
+     * Returns `undefined` if the database is not connected.
+     * @param {string} key The key to check.
+     * @returns {boolean | undefined}
+     * @example
+     * store.has("key"); // false
+     * store.set("key", "value");
+     * store.has("key"); // true
+     */
+    has(key: string): boolean | undefined {
+        if (!this.connected) return undefined;
+        return key in this.store;
+    }
+
+    /**
+     * Sets a value in the database.
+     * Returns `undefined` if the database is not connected.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {string} key The key to set.
+     * @param {T} value The value to set.
+     * @returns {boolean | undefined}
+     * @example
+     * store.set("key", "value");
+     * store.get("key"); // "value"
+     */
+    set<T extends unknown>(key: string, value: T): boolean | undefined {
+        if (!this.connected) return undefined;
+
+        const before = { ...this.store };
+        this.store[key] = value;
+        return this.update(before);
+    }
+
+    /**
+     * Copies a key from another Store.
+     * If a fallback is provided, it will be set in the database and returned.
+     * Returns `undefined` if either database is not connected.
+     * Returns `null` if the key does not exist in the other Store.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {Store<Y>} store The Store to copy from.
+     * @param {string} key The key to copy.
+     * @param {Z} fallback The fallback value to use. Optional.
+     * @returns {boolean | null | undefined}
+     * @example
+     * store2.set("key", "value");
+     * store.copyKey(store2, "key"); // true
+     * store.get("key"); // "value"
+     */
+    copyKey<Z extends unknown, Y extends {} = {}>(store: Store<Y>, key: string, fallback?: Z): boolean | null | undefined {
+        if (!this.connected || !store.connected) return undefined;
+
+        const value = store.get(key, fallback);
+        if (value === null) return null;
+        return this.set(key, value as unknown as T[keyof T]);
+    }
+
+    /**
+     * Copies all keys from another Store.
+     * Returns `undefined` if either database is not connected.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {Store<Y>} store The Store to copy from.
+     * @param {boolean} clean Whether to clear the database before copying. Optional.
+     * @returns {boolean | undefined}
+     * @example
+     * store2.set("key", "value");
+     * store.copyFrom(store2);
+     * store.get("key"); // "value"
+     */
+    copyFrom<Y extends {} = {}>(store: Store<Y>, clean?: boolean): boolean | undefined {
+        if (!this.connected || !store.connected) return undefined;
+
+        const before = { ...this.store };
+        if (clean) this.store = {} as T;
+        for (const key in store.internalStore) {
+            this.store[key as string] = store.internalStore[key];
+        }
+
+        return this.update(before);
+    }
+
+    /**
+     * Updates the database. Primarily used internally.
+     * Returns `undefined` if the database is not connected.
+     * If the operation fails, the database is reverted and `false` is returned.
+     * @param {T} before The state of the database before the update. Optional.
+     * @returns {boolean | undefined}
+     * @example
+     * store.store = { key: "value" };
+     * store.update();
+     */
+    protected update(before?: T): boolean | undefined {
+        if (!this.connected) return undefined;
+        if (this.inDevMode) return true;
+        let failed = false;
+
+        this.keyv!.set("store", this.store).catch(() => {
+            if (before) this.store = before;
+            failed = true;
+        });
+
+        return !failed;
+    }
+}
